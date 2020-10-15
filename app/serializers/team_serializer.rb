9834b0e83cf8d182109fde702148767a6ef3cacf
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-10-15 16:49:06 -0400

    mvp

--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,5 +1,5 @@
 class TeamSerializer < ActiveModel::Serializer
-    attributes :id, :name, :user_id, :league_id
+    attributes :id, :name, :user_id, :league_id, :num_of_points
     has_many :contracts
     has_many :players
     has_one :league
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_11_171903) do
+ActiveRecord::Schema.define(version: 2020_10_15_152711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2020_10_11_171903) do
     t.integer "league_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "num_of_points", default: 0
   end
 
   create_table "users", force: :cascade do |t|
